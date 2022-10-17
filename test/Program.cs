de68fafa03dabcd90afc3eb500c3994db186e8f5
Anastasia <silutina.n@yandex.ru>
2022-10-18 00:01:03 +0300

    6zadanie

---
+++ test/Program.cs
@@ -0,0 +1,28 @@
+﻿//6. Функцию, определяющую является ли число простым, 
+//то есть возвращающую true, если число простое, иначе - false
+
+using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число = ");
+        int n = int.Parse(Console.ReadLine());
+ 
+        bool isPrime;
+        do
+        {
+            isPrime = true;
+            double sqrt = Math.Sqrt(n);
+            for (int i = 2; i <= sqrt; i++)
+                if (n % i == 0)
+                    isPrime = false;
+            if (!isPrime)
+                n++;
+        }
+        while (!isPrime);
+ 
+        Console.WriteLine("Простое " + n);
+    }
+}
